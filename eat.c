83f817f4c478db6e71d4321ba64d08d2322b19e6
denis <denis.c1@live.fr>
2021-12-16 14:27:59 +0100

    pb death

--- eat.c
+++ eat.c
@@ -6,21 +6,19 @@
 /*   By: dchheang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 09:36:04 by dchheang          #+#    #+#             */
-/*   Updated: 2021/12/16 13:22:59 by dchheang         ###   ########.fr       */
+/*   Updated: 2021/12/16 14:21:15 by dchheang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	ft_sleep(t_info *info, int t)
+int	ft_sleep(t_info *info, unsigned long t)
 {
-	int	i;
+	unsigned long	start;
 
-	i = 0;
-	while (1)
+	start = get_time();
+	while (get_timediff(start) < t)
 	{
-		if (i >= t)
-			break ;
 		pthread_mutex_lock(&info->eat_mutex);
 		if (info->end_sim)
 		{
@@ -28,20 +26,27 @@ int	ft_sleep(t_info *info, int t)
 			return (0);
 		}
 		pthread_mutex_unlock(&info->eat_mutex);
-		i++;
-		usleep(1000);
+		usleep(1);
 	}
 	return (1);
 }
 
 void	take_forks(t_philo *philo)
 {
-	pthread_mutex_lock(philo->rf);
-	print_status(philo, philo->info, "has taken a fork");
-	pthread_mutex_unlock(philo->rf);
-	pthread_mutex_lock(philo->lf);
-	print_status(philo, philo->info, "has taken a fork");
-	pthread_mutex_unlock(philo->lf);
+	if (philo->id % 2)
+	{
+		pthread_mutex_lock(philo->lf);
+		print_status(philo, philo->info, "has taken a fork");
+		pthread_mutex_lock(philo->rf);
+		print_status(philo, philo->info, "has taken a fork");
+	}
+	else
+	{
+		pthread_mutex_lock(philo->rf);
+		print_status(philo, philo->info, "has taken a fork");
+		pthread_mutex_lock(philo->lf);
+		print_status(philo, philo->info, "has taken a fork");
+	}
 }
 
 void	drop_forks(t_philo *philo)
@@ -59,6 +64,7 @@ void	eat(t_philo *philo)
 	print_status(philo, philo->info, "is eating");
 	if (ft_sleep(philo->info, philo->info->time_to_eat))
 		print_status(philo, philo->info, "is sleeping");
+	drop_forks(philo);
 	if (ft_sleep(philo->info, philo->info->time_to_sleep))
 		print_status(philo, philo->info, "is thinking");
 }
