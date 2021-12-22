34ad3c8a9219bdeeeef3e2fc09e2db04b310fe15
Denis CHHEANG <dchheang@e3r6p17.clusters.42paris.fr>
2021-12-22 16:49:57 +0100

    pb impair

--- eat.c
+++ eat.c
@@ -6,7 +6,7 @@
 /*   By: dchheang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 09:36:04 by dchheang          #+#    #+#             */
-/*   Updated: 2021/12/22 16:16:44 by dchheang         ###   ########.fr       */
+/*   Updated: 2021/12/22 16:48:03 by dchheang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,38 +25,26 @@ void	ft_sleep(t_philo *philo, unsigned long t)
 	}
 }
 
-int		take_fork(t_philo *philo, pthread_mutex_t *mutex)
-{
-	pthread_mutex_lock(mutex);
-	if (check_end_sim(philo, philo->info))
-	{
-		pthread_mutex_unlock(mutex);
-		return (0);
-	}
-	return (1);
-}
-
 void	take_forks(t_philo *philo)
 {
 	if (philo->id % 2)
 	{
-		if (take_fork(philo, philo->lf))
+		pthread_mutex_lock(philo->lf);
+		print_status(philo, philo->info, "has taken a fork");
+		if (philo->info->n_philo == 1)
+			ft_sleep(philo, philo->info->time_to_die);
+		else
 		{
+			pthread_mutex_lock(philo->rf);
 			print_status(philo, philo->info, "has taken a fork");
-			if (philo->info->n_philo == 1)
-				ft_sleep(philo, philo->info->time_to_die);
-			else if (take_fork(philo, philo->rf))
-				print_status(philo, philo->info, "has taken a fork");
 		}
 	}
 	else
 	{
-		if (take_fork(philo, philo->rf))
-		{
-			print_status(philo, philo->info, "has taken a fork");
-			if (take_fork(philo, philo->lf))
-				print_status(philo, philo->info, "has taken a fork");
-		}
+		pthread_mutex_lock(philo->rf);
+		print_status(philo, philo->info, "has taken a fork");
+		pthread_mutex_lock(philo->lf);
+		print_status(philo, philo->info, "has taken a fork");
 	}
 }
 
@@ -69,8 +57,8 @@ void	drop_forks(t_philo *philo)
 	}
 	else
 	{
-		pthread_mutex_unlock(philo->rf);
 		pthread_mutex_unlock(philo->lf);
+		pthread_mutex_unlock(philo->rf);
 	}
 }
 
